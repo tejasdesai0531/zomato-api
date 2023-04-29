05a86acb1052787108da979afce3b88776691eb9
Tejas Desai <tejadesai0531@gmail.com>
2023-04-29 23:16:00 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const app = require('./app')
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 mongoose.connect(process.env.DB_URL)
